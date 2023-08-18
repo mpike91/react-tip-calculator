d6f7d548198b72a53eb5b68828f4ff0e5db8d0d4
Matt Pike <mattp91@gmail.com>
2023-08-18 17:06:58 -0400

    Finished challenge to create tip calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const initialService = { 0: 0, 1: 0 };
 
 function App() {
+  const [bill, setBill] = useState(100);
+  const [service, setService] = useState(initialService);
+
+  function handleChangeService(id, value) {
+    setService((s) => ({ ...s, [id]: Number(value) }));
+  }
+
+  function handleReset() {
+    setBill(100);
+    setService(initialService);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bill bill={bill} setBill={setBill} />
+      <Service
+        id={0}
+        service={service}
+        handleChangeService={handleChangeService}
+      >
+        <span>How did you like the service?</span>
+      </Service>
+      <Service
+        id={1}
+        service={service}
+        handleChangeService={handleChangeService}
+      >
+        <span>How did your friend like the service?</span>
+      </Service>
+      <Payment bill={bill} service={service} />
+      <Button handleReset={handleReset}>Reset</Button>
+    </div>
+  );
+}
+
+function Bill({ bill, setBill }) {
+  return (
+    <div>
+      <label>
+        <span>How much was the bill? </span>
+        <input
+          type="text"
+          value={bill}
+          onChange={(e) => setBill(Number(e.target.value))}
+        />
+      </label>
+    </div>
+  );
+}
+
+function Service({ id, service, handleChangeService, children }) {
+  return (
+    <div>
+      <span>{children} </span>
+      <select
+        value={service[id]}
+        onChange={(e) => handleChangeService(id, e.target.value)}
+      >
+        <option value={0}>Dissatisfied (0%)</option>
+        <option value={5}>It was okay (5%)</option>
+        <option value={10}>It was good (10%)</option>
+        <option value={20}>Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Payment({ bill, service }) {
+  const values = Object.values(service);
+  const average = values.reduce((acc, el) => acc + el, 0) / values.length;
+  const tip = Math.round((bill / 100) * average);
+  return (
+    <div>
+      <h3>
+        You pay ${bill + tip} (${bill} + ${tip} tip)
+      </h3>
+    </div>
+  );
+}
+
+function Button({ handleReset, children }) {
+  return (
+    <div>
+      <button onClick={handleReset}>{children}</button>
     </div>
   );
 }
